a0bf78e1cd851917c2aa68ac9bcf19f12835cb98
Kyawawa <tkmz0216@gmail.com>
2020-03-23 00:24:42 +0900

    test version 1

--- Arduino/key_arduino.ino
+++ Arduino/key_arduino.ino
@@ -2,7 +2,7 @@
 
 Servo servo;
 const int door_read_pin = 8;
-int serial = 0;
+int door_status = 0;
 
 void setup() {
   pinMode(door_read_pin,INPUT);
@@ -13,6 +13,38 @@ void setup() {
   
 }
 
-void loop() { 
+
+
+void loop() {
+  byte var;
+  var = Serial.read();
+  switch(var){
+    case '0':
+      servo.write(0);
+      Serial.write(0);
+      delay(10000);
+      
+      break;
+    case '1':
+      servo.write(90);
+      Serial.write(1);
+      
+      break;
+    default:
+      break;
+  }
+
+  int door_digital_read = digitalRead(door_read_pin);
+
+  if(door_digital_read > 0){
+    delay(100);
+    Serial.print(1);
+    servo.write(90);
+    delay(100);
+  }else{
+    Serial.print(0);
+    servo.write(0);
+    delay(100);
+  }
+
   
-}
