8e28b5891e43c622c9e20c77b13c54598d790e54
tefu <tefu@tefus-MacBook-Air.local>
2020-03-24 23:48:41 +0900

    front-end

---
+++ app/login_key.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template, url_for
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    status = False #本当は電流が流れているかから読み取る
+    if request.method == 'POST':
+        print("POSTされたIDは？" + str(request.form['id']))
+        print("POSTされたPASSWORDは？" + str(request.form['pwd']))
+        if str(request.form['id'])==idid and str(request.form['pwd'])==ps:
+            if status==False:
+                ###
+                #ロック解除の処理をここに書く
+                ###
+                status = True #本当は電流が流れているかから読み取る
+                return render_template('opened.html')
+            else:
+                return render_template('already_opened.html')
+        else:
+            return render_template('not_opened.html', status="ロックされていません！！")
+
+    else:
+        if status==True:
+            return render_template('index.html', status="OK")
+        else:
+            return render_template('index.html', status="ロックされていません！！")
+
+if __name__ == "__main__":
+    #app.run(port = 8000, debug= True)
+    idid = 'aaa'
+    ps = 'bbb'
+    app.run(host='0.0.0.0', debug= True)
