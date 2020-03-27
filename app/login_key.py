78bce3b056285df5f1afbb82b75f3b94b3214a9c
tehhuu <volley_neverlose_exile@yahoo.co.jp>
2020-03-28 04:45:03 +0900

    pyserialを組み込んだ

--- app/login_key.py
+++ app/login_key.py
@@ -1,21 +1,41 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, url_for, redirect
+import serial,time
 import os
+
+def open_key(): #鍵を開ける
+    ser = serial.Serial('COM6',9600)
+    ser.write(b"1")
+
+def get_status(): #鍵が開いているかの状態を取得
+    ser = serial.Serial('COM6',9600)
+    key_status = ser.read()
+
+    def b_to_bool(ser): #バイナリをTrue or Falseに変換
+        if ser == b'0':
+            return False
+        else:
+            return True
+    
+    return b_to_bool(key_status)
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
-    status = False #本当は電流が流れているかから読み取る
+    #status = False
+    status = get_status() #鍵の状態を読み取り
     if request.method == 'POST':
         print("POSTされたIDは？" + str(request.form['id']))
         print("POSTされたPASSWORDは？" + str(request.form['pwd']))
         if str(request.form['id'])==idid and str(request.form['pwd'])==ps:
             if status==False:
-                ###
-                #ロック解除の処理をここに書く
-                ###
-                status = True #本当は電流が流れているかから読み取る
-                return render_template('opened.html')
+                open_key() #鍵を開ける。
+                status = get_status()
+                #status = True
+                if status != True:
+                    return render_template('error.html')
+                else:
+                    return render_template('opened.html')
             else:
                 return render_template('already_opened.html')
         else:
@@ -28,6 +48,7 @@ def form():
             return render_template('index.html', status="ロックされていません！！")
 
 if __name__ == "__main__":
-    idid = 'aaa'
-    ps = 'bbb'
+    app.secret_key = os.urandom(12)
+    idid = 'a' #適宜設定
+    ps = 'b' #適宜設定
     app.run(host='0.0.0.0', debug= True)
