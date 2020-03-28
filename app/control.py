87d619ba762e4633687f0c72a84fa65012df0451
Kyawawa <tkmz0216@gmail.com>
2020-03-28 14:48:50 +0900

    fix bug

--- app/control.py
+++ app/control.py
@@ -2,10 +2,11 @@ import serial,time
 
 def open_key():
     ser = serial.Serial('COM6',9600)
-    ser.write(b"1")
+    ser.write(b'1')
 
 def get_status():
     ser = serial.Serial('COM6',9600)
     key_status = ser.read()
     return key_status
+
 
